e495ceaaf4f352fed05a76ca090b4752277869a1
chandan0c <chandan0c098@gmail.com>
2022-09-10 14:20:58 +0530

    image problem resolved

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -14,7 +14,7 @@ export const ProductDetails=()=>{
 
     return <>
        <div>
-        <img src={data.image} alt="image"></img>
+        <img src={data.image} alt="product-image"></img>
         <h1>{data.brand}</h1>
         <h2>{data.title}</h2>
         <h3>{data.category}</h3>
